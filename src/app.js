a7771d236aef9c203b3002902ec7b3eea5ea8d5a
Ahmed Ramsey <54491254+ramsey8057@users.noreply.github.com>
2019-12-16 23:52:33 +0200

    adding automatic port

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const routes = require('./routes/routes');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
