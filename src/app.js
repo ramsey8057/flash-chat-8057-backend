2805b3411de65c1825556e3c15484a15f04c4d57
Ahmed Ramsey <54491254+ramsey8057@users.noreply.github.com>
2019-12-16 23:53:15 +0200

    Making port choise better

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const routes = require('./routes/routes');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
